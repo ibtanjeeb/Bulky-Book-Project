3bc7fcb14901642668cb46637f625fa3fb7bb656
ibTanjeeb <threepointonefour1993@gmail.com>
2022-02-18 16:10:45 +0600

    Added IUnitOfWork.cs

---
+++ BulkyBook/BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+   public interface IUnitOfWork:IDisposable
+    {
+        public ICategoryRepository Category { get; }
+        public ISP_Call SP_Call { get; }
+
+    }
+}
