043f86e9606b35c1aa23b07fa8b0053ba136d8d4
ibTanjeeb <threepointonefour1993@gmail.com>
2022-02-19 19:50:42 +0600

    Added Category Upsert

---
+++ BulkyBook/BulkyBook/Controllers/CategoryController.cs
@@ -0,0 +1,60 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+   
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Upsert(int? id)
+        {
+            Category category = new Category();
+
+            if(id==null)
+            {
+                return View(category);
+            }
+
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+            
+            if(category==null)
+            {
+                return NotFound();
+            }
+            return View(category);
+            return View();
+        }
+
+
+        #region API CAlls
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+            var allobj = _unitOfWork.Category.GetAll();
+
+            return Json(new { data = allobj });
+        }
+
+
+        #endregion
+    }
+}
