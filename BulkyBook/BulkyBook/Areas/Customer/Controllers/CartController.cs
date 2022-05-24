dd080d17115b55a1c711158b286f968324c361c3
ibTanjeeb <threepointonefour1993@gmail.com>
2022-05-25 01:51:43 +0600

    Added 14. Process Delayed Payments

--- BulkyBook/BulkyBook/Areas/Customer/Controllers/CartController.cs
+++ BulkyBook/BulkyBook/Areas/Customer/Controllers/CartController.cs
@@ -185,6 +185,10 @@ namespace BulkyBook.Areas.Customer.Controllers
 
             if(stripeToken==null)
             {
+                //Order Will Payment  Delayed For Authorization Company
+                ShoppingCartVM.OrderHeader.PaymentDueDate = DateTime.Now.AddDays(30);
+                ShoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusDelayedPayment;
+                ShoppingCartVM.OrderHeader.OrderStatus = SD.PaymentStatusApproved;
 
             }
             else
