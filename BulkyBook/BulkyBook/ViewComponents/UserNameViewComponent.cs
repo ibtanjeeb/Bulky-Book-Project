0540dda577e30c9e1b8f7275334509fd21cf2a17
ibTanjeeb <threepointonefour1993@gmail.com>
2022-05-29 22:52:32 +0600

    Added ViewComponents/UserNameViewComponent.cs Views/Shared/Components/UserName/Default.cshtml Views/Shared/Components/UserName/Default.cshtml.g.cs

---
+++ BulkyBook/BulkyBook/ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.ViewComponents
+{
+    public class UserNameViewComponent:ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public UserNameViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimIdentity = (ClaimsIdentity)User.Identity;
+            var claims = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var UserFromDb = _unitOfWork.applicationUser.GetFirstOrDefault(u => u.Id == claims.Value);
+
+            return View(UserFromDb);
+
+        }
+    }
+}
